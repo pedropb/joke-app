b81109cfbcc93afdd80907e06025ff75edb216da
Pedro Baracho <pedropbaracho@gmail.com>
2016-12-12 21:49:12 -0200

    Added Joke Component

---
+++ src/app/joke/joke.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Joke } from '../joke';
+
+@Component({
+  selector: 'joke',
+  templateUrl: './joke.component.html'
+})
+export class JokeComponent {
+
+  @Input('joke') data: Joke;
+  @Output() jokeDeleted = new EventEmitter<Joke>();
+
+  deleteItem() {
+    this.jokeDeleted.emit(this.data);
+  }
+}
