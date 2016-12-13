81fbf3277075f97c46520ffe24be03a2d77f5fa6
Pedro Baracho <pedropbaracho@gmail.com>
2016-12-12 22:05:38 -0200

    Added JokeListComponent Initialize AppComponent with JokeListComponent. Added stub for JokeFormComponent

---
+++ src/app/joke-form/joke-form.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-joke-form',
+  templateUrl: './joke-form.component.html'
+})
+export class JokeFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/joke-list/joke-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { JokeComponent } from '../joke/joke.component';
+import { Joke } from '../joke';
+
+
+@Component({
+  selector: 'joke-list',
+  templateUrl: './joke-list.component.html'
+})
+export class JokeListComponent {
+
+  jokes: Joke[];
+
+  constructor() {
+    this.jokes = [
+      new Joke("What did the cheese say when it looked in the mirror?", "Hello-me (Halloumi)"),
+      new Joke("What kind of cheese do you use to disguise a small horse?", "Mask-a-pony (Mascarpone)"),
+      new Joke("A kid threw a lump of cheddar at me", "I thought ‘That’s not very mature’"),
+    ];
+   }
+
+  public addJoke(joke: Joke) {
+    this.jokes.unshift(joke);
+  }
+
+  public deleteJoke(joke: Joke) {
+    let indexToDelete = this.jokes.indexOf(joke);
+    if (indexToDelete !== -1) {
+      this.jokes.splice(indexToDelete,1);
+    }
+  }
+
+}
