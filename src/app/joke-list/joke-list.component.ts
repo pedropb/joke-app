9855e8631d75ae5e628df7b02d35a9ca280a2e20
Pedro Baracho <pedropbaracho@gmail.com>
2016-12-12 22:06:31 -0200

    Removed OnInit

--- src/app/joke-list/joke-list.component.ts
+++ src/app/joke-list/joke-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { JokeComponent } from '../joke/joke.component';
 import { Joke } from '../joke';
 
